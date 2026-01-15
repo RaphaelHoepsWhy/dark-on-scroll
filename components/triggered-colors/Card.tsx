7d1b90e74b0c8e8b7be429701e0f72819b1cf52e
RaphaelHoepsWhy <raphael.hoeps@why.de>
2026-01-15 11:07:57 +0100

    feat: transition

--- components/triggered-colors/Card.tsx
+++ components/triggered-colors/Card.tsx
@@ -5,7 +5,7 @@ type CardProps = {
 
 export function Card({ title, description }: CardProps) {
   return (
-    <div className="rounded-2xl border border-pink-400 bg-pink-400/40 p-6 text-black transition-colors duration-700 dark:border-slate-700 dark:bg-slate-700/50 dark:text-slate-200">
+    <div className="rounded-2xl border border-pink-400 bg-pink-400/40 p-6 text-black transition-colors duration-700 ease-in-out dark:border-slate-700 dark:bg-slate-700/50 dark:text-slate-200">
       <h3 className="mb-3 text-xl font-semibold">{title}</h3>
       <p className="leading-relaxed opacity-70">{description}</p>
     </div>
